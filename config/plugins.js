81f9fa7c352b8cc6989342d4e5df9698e0c35fef
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-03-29 00:39:01 +0100

    feat: new field in user collection | removed email

--- config/plugins.js
+++ config/plugins.js
@@ -16,23 +16,23 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  email: {
-    config: {
-      provider: "nodemailer",
-      providerOptions: {
-        host: env("SMTP_HOST", "smtp.example.com"),
-        port: env("SMTP_PORT", 587),
-        auth: {
-          user: env("SMTP_USERNAME"),
-          pass: env("SMTP_PASSWORD"),
-        },
-        // ... any custom nodemailer options
-      },
-      settings: {
-        defaultFrom: "ibk2k7@gmail.com",
-        defaultReplyTo: "ibk2k7@gmail.com",
-      },
-    },
-  },
+  // email: {
+  //   config: {
+  //     provider: "nodemailer",
+  //     providerOptions: {
+  //       host: env("SMTP_HOST", "smtp.example.com"),
+  //       port: env("SMTP_PORT", 587),
+  //       auth: {
+  //         user: env("SMTP_USERNAME"),
+  //         pass: env("SMTP_PASSWORD"),
+  //       },
+  //       // ... any custom nodemailer options
+  //     },
+  //     settings: {
+  //       defaultFrom: "ibk2k7@gmail.com",
+  //       defaultReplyTo: "ibk2k7@gmail.com",
+  //     },
+  //   },
+  // },
   // ...
 });
